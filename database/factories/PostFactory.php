d59beb0d1c1bf438604d3370487fc477ebb6ac91
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-11-12 22:11:45 +0100

    create post model and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+  
+    public function definition(): array
+    {
+        return [
+         "title"=>$this->faker->title,
+         "body"=> $this->faker->paragraph,
+         "created_at"=>now()
+        ];
+    }
+}
