755b41f9aa306834fec3b1d8ab035688a35a75f7
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-11-13 11:54:51 +0100

    post and get  cars

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cars;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    
+    public function index(){
+        $cars = Cars::all();
+        return response($cars) ;
+    }
+    public function store(Request $request){
+        $validated = $request->validate( [
+          "name"=>"required",
+            "founded"=>"required",
+            "desc"=>"required"
+        ]
+            
+        );
+        $cars =  Cars::create($validated);
+        return response($cars);
+
+    }
+
 }
